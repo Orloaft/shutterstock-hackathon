caa831abf87bf50ac87d67fda7155ac82265ce99
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-13 09:14:19 -0400

    added photos

--- client/src/pages/InspirePage/InspirePage.js
+++ client/src/pages/InspirePage/InspirePage.js
@@ -1,8 +1,31 @@
 import "./InspirePage.scss";
 import navbartop from "../../assets/images/navbartop.png";
 import navbarside from "../../assets/images/sidenavbar.png";
-import ace from "../../assets/images/ace.svg";
-const imageArray = [ace, ace, ace, ace, ace, ace, ace, ace, ace];
+import allthings from "../../assets/images/allthings.png";
+import appNyc from "../../assets/images/app_nyc.png";
+import benjaminBoat from "../../assets/images/benjamin-boat.jpg";
+import benjaminLake from "../../assets/images/benjamin-lake.jpg";
+import davidClode from "../../assets/images/david-clode.jpg"
+import elephant from "../../assets/images/elephant.png";
+import exploreNorway from "../../assets/images/explore_norway.png";
+import howlingRed from "../../assets/images/howling-red.jpg";
+import joshConnor from "../../assets/images/josh-connor.jpg";
+import kimsonDoan from "../../assets/images/kimson-doan.jpg";
+import madsSchemidt from "../../assets/images/mads-schmidt.jpg";
+import matthewHamilton from "../../assets/images/matthew-hamilton.jpg";
+import memories from "../../assets/images/memories.jpg";
+import mobilePrint from "../../assets/images/mobile-printing.jpeg";
+import modernGirl from "../../assets/images/modern_browngirl.png";
+import mountainApp from "../../assets/images/mountain_app.png";
+import plantPage from "../../assets/images/plantsandjars.png";
+import poto from "../../assets/images/poto.jpg"
+import sanderDon from "../../assets/images/sander-don.jpg";
+import sarahDor from "../../assets/images/sarah-dorweiler.jpg";
+import sebastianTurtle from "../../assets/images/sebastian-turtle.gif"; 
+import srilanka from "../../assets/images/sri_lanka.png";
+import turtle from "../../assets/images/turtle.jpg";
+
+const imageArray = [mountainApp, allthings, srilanka, memories, appNyc, sebastianTurtle, elephant, exploreNorway, poto];
 
 export const InspirePage = ({ handleClick }) => {
   return (
