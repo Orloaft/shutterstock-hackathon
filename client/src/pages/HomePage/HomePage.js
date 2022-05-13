9bdab47096e8061725035a083aecf9834747a06d
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-12 23:41:11 -0400

    link, some changes in homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -5,15 +5,18 @@ import navbarside from '../../assets/images/sidenavbar.png'
 import hero from '../../assets/images/hero.png';
 import body from '../../assets/images/body.png';
 import holder from '../../assets/images/inspire_placeholder.jpg'
+import {Link} from 'react-router-dom';
 
 function HomePage(props) {
     return (
         <>
+            <header className='navbar'>
             <img className='navbar__top' src={navbartop} />
             <img />
             <nav className='navbar__left-container'>
             <img className='navbar__left' src={navbarside}/>
             </nav>
+            </header>
             <main>
             <img className='hero' src={hero} />
             <article className='inspire'>
@@ -23,7 +26,9 @@ function HomePage(props) {
                     <img className='inspire__img' src={holder}/>
                     <img className='inspire__img' src={holder}/>
                 </div>
+                <Link to='/inspiration'>
                 <button className='inspire__button'>Get Inspired</button>
+                </Link>
             </article>
             <img className='main' src={body}/>
 
