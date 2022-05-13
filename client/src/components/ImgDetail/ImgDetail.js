4a3296b9b49e000f8e320fa78c2cc88a64143ac3
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-13 11:58:37 -0400

    added to detail pop up

--- client/src/components/ImgDetail/ImgDetail.js
+++ client/src/components/ImgDetail/ImgDetail.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
-function ImgDetail() {
+function ImgDetail({handleClick}) {
     return (
         <div>
             <h3>Download this image</h3>
             <img className='detail__image'/>
-            <details>
+            <div>
                 <h4 className='detail__title'>Image size / Format</h4>
                     <div className='detail__container'>
                         <p>size info goes here</p>
                     </div>
-                <h4 className='detail__title'>Creator</h4>
-                <img className='detail__avatar'/> 
-                <p>creator name</p>
+                    <div>
+                        <p>Item Id: 2124865324</p>
+                        <p>Creator: creator name</p>
+                        <a href='https://www.shutterstock.com/pricing'>View our pricing and subscription plans</a>
+                    </div>
+                    <div className='detail__buttons-container'>
+                        <button>Download</button>
+                        <button onClick={handleClick}>Cancel</button>
 
+                    </div>
 
-            </details>
+            </div>
             
         </div>
     );
