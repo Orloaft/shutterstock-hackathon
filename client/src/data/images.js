b9fa4c28ae9b346a2f0a6a3ea778bbc195aec112
Orloaft <alexorlow17@gmail.com>
2022-05-13 13:00:36 -0400

    add separate file to store array of image data

---
+++ client/src/data/images.js
@@ -0,0 +1,43 @@
+import allthings from "../assets/images/allthings.png";
+import appNyc from "../assets/images/app_nyc.png";
+import exploreNorwayStock from "../assets/images/benjamin-boat.jpg";
+import allthingsStock from "../assets/images/benjamin-lake.jpg";
+import elephantStock from "../assets/images/david-clode.jpg";
+import elephant from "../assets/images/elephant.png";
+import exploreNorway from "../assets/images/explore_norway.png";
+import potoStock from "../assets/images/howling-red.jpg";
+import appNycStock from "../assets/images/josh-connor.jpg";
+import kimsonDoan from "../assets/images/kimson-doan.jpg";
+import mountainAppStock from "../assets/images/mads-schmidt.jpg";
+import matthewHamilton from "../assets/images/matthew-hamilton.jpg";
+import memories from "../assets/images/memories.jpg";
+import memoriesStock from "../assets/images/mobile-printing.jpeg";
+import modernGirl from "../assets/images/modern_browngirl.png";
+import mountainApp from "../assets/images/mountain_app.png";
+import plantPage from "../assets/images/plantsandjars.png";
+import poto from "../assets/images/poto.jpg";
+import srilankaStock from "../assets/images/sander-don.jpg";
+import sarahDor from "../assets/images/sarah-dorweiler.jpg";
+import sebastianTurtle from "../assets/images/sebastian-turtle.gif";
+import srilanka from "../assets/images/sri_lanka.png";
+import sebastianTurtleStock from "../assets/images/turtle.jpg";
+
+export const objArray = [
+  { author: "Mads Shmidt", stock: mountainAppStock, product: mountainApp },
+  { author: "Benjamin Lake", stock: allthingsStock, product: allthings },
+  { author: "Sander Don", stock: srilankaStock, product: srilanka },
+  { author: "Robert Wallace", stock: memoriesStock, product: memories },
+  { author: "Josh Connor", stock: appNycStock, product: appNyc },
+  {
+    author: "Sebastian Turtle",
+    stock: sebastianTurtleStock,
+    product: sebastianTurtle,
+  },
+  { author: "David Clode", stock: elephantStock, product: elephant },
+  {
+    author: "Benjamin Lake",
+    stock: exploreNorwayStock,
+    product: exploreNorway,
+  },
+  { author: "Sam Smith", stock: potoStock, product: poto },
+];
