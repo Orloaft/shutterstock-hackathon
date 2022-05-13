a400f0360a255ece21a8a64a0d1fc0969e356c75
Orloaft <alexorlow17@gmail.com>
2022-05-13 00:38:10 -0400

    add close button to modal

--- client/src/pages/InspirePage/InspirePage.js
+++ client/src/pages/InspirePage/InspirePage.js
@@ -1,8 +1,8 @@
 import "./InspirePage.scss";
 import navbartop from "../../assets/images/navbartop.png";
 import navbarside from "../../assets/images/sidenavbar.png";
-import icon from "../../assets/images/icon.png";
-const imageArray = [icon, icon, icon, icon, icon, icon, icon, icon, icon];
+import ace from "../../assets/images/ace.svg";
+const imageArray = [ace, ace, ace, ace, ace, ace, ace, ace, ace];
 
 export const InspirePage = ({ handleClick }) => {
   return (
@@ -17,6 +17,7 @@ export const InspirePage = ({ handleClick }) => {
           ></input>
         </div>
         <div className="inspiration__gallery">
+          <h2 className="inspiration__gallery-heading"> Featured Creators</h2>
           {imageArray.map((img) => {
             return (
               <img
@@ -25,7 +26,7 @@ export const InspirePage = ({ handleClick }) => {
                 }}
                 className="inspiration__image"
                 src={img}
-                alt="shutterstock icon"
+                alt="shutterstock ace"
               ></img>
             );
           })}
