41d6abb5aabe9d333714ebfb55a6157ccd882010
Alisa Promthep <alisa.promthep@gmail.com>
2022-05-12 23:22:43 -0400

    homepage with place holders

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -11,19 +11,21 @@ function HomePage(props) {
         <>
             <img className='navbar__top' src={navbartop} />
             <img />
+            <nav className='navbar__left-container'>
             <img className='navbar__left' src={navbarside}/>
+            </nav>
             <main>
-            <img className='hero' src='{hero}' />
-            <article className='inspired'>
-                <h2>Creations</h2>
-                <div>
-                    <img className='inspire__img' src='holder' />
-                    <img className='inspire__img' src='holder'/>
-                    <img className='inspire__img' src='holder'/>
-                    <button>Get Inspired</button>
+            <img className='hero' src={hero} />
+            <article className='inspire'>
+                <h2 className='inspire__title'>Featured Creators</h2>
+                <div className='inspire__container'>
+                    <img className='inspire__img' src={holder} />
+                    <img className='inspire__img' src={holder}/>
+                    <img className='inspire__img' src={holder}/>
                 </div>
-
+                <button className='inspire__button'>Get Inspired</button>
             </article>
+            <img className='main' src={body}/>
 
             </main>
 
